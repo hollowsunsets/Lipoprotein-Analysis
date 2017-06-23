8706e438de211dee232fd0de0a6d5fbd1da443c9
Isabel Giang <Isabel Giang>
2017-06-23 11:53:48 -0700

    finished dropScan, addBackScan

--- other/graph-alteration.R
+++ other/graph-alteration.R
@@ -1,26 +1,37 @@
 library(dplyr)
 
-# global var that tracks the scans that were dropped 
+# ----------------- Global Variables ---------------------------
 scansDropped <- c(0)
 
 
+# -------------------- Functions -------------------------------
+# Drops the scan that corresponds with the given scan number from the 
+# given dataset. Produces error message if the given scan number does not
+# correspond with a column in the dataset.
 dropScan <- function(graph.data, scan.number) {
   if (!(scan.number %in% colnames(graph.data))) {
     stop()
   }
   # Selects all columns that are not the given scan number 
-  
   graph.data <- select(graph.data, which(colnames(graph.data) != scan.number)) 
   # Tracks the number of the scan that was dropped
   scansDropped[length(scansDropped) + 1] <- scan.number
   return(graph.data)
 }
 
-addScan <- function(graph.data, scan.number, original.data) {
-  if (!(scan.number %in% scansDropped)) {
+# Adds the given scan number from the original data to the graphed dataset.
+# Error message is produced if the scan has not been dropped previously
+# from the dataset or if there is no corresponding column in the original, unmodified dataset.
+addBackScan <- function(graph.data, scan.number, original.data) {
+  if ((!(scan.number %in% scansDropped)) || (!(scan.number %in% colnames(original.data)))) {
     stop()
   }
+  # Adds the column that corresponds with the given scan number back to the graphed dataset
   cbind(graph.data, original.data$scan.number)
+  # Removes the number tracking the scan that was previously dropped 
+  # i.e, scansDropped = 1 3 4, scan.number = 1
+  # post: scansDropped = 3 4
+  scansDropped <- scansDropped[-which(scansDropped %in% scan.number)]
   return(graph.data)
 }
 
