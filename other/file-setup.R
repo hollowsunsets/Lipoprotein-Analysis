db496d82373fac4f5b97b2fefcf32873a0a9d586
Isabel Giang <Isabel Giang>
2017-06-24 20:29:05 -0700

    updated file-setup.R

--- other/file-setup.R
+++ other/file-setup.R
@@ -7,25 +7,43 @@ sparklink.file <- read.csv("data\\SparkRunlistDataset2.csv", stringsAsFactors = 
 scans.file <- read.csv("data\\AIMDataset2.csv", stringsAsFactors = FALSE)
 amplog.file <- read.xlsx("data\\ampdDataset2.xlsx", sheetIndex = 1, as.data.frame = T, header = F, stringsAsFactors=FALSE)
 
-# Returns 
+# With the given Sparklink and scan files, returns a data frame that has 
+# scan data, 
 scanGraphData <- function(raw.sparklink.file, raw.scans.file) {
   
   # Getting relevant data from Sparklink file
   scan.graph.data <- raw.sparklink.file %>% select(Sample.Name, Sample.Vial, Executed)
   
   # Removing rows before relevant scan data begins from scans file 
-  scans.test <- scans.file[which(scans.file$Sample.File == "Raw Data - Time(s)"):nrow(scans.file),]
+  raw.scans.df <- scans.file[which(scans.file$Sample.File == "Raw Data - Time(s)"):nrow(scans.file),]
 
   # Change column names to the first row (which contains names of relevant data)
-  colnames(scans.test) <- scans.test[1,]
-  scans.test <- scans.test[-1,]
+  colnames(raw.scans.df) <- raw.scans.df[1,]
+  raw.scans.df <- raw.scans.df[-1,]
   
+  # Filter out data that is not relevant to scans (gets all count data)
+  filtered.scans <- raw.scans.df %>% select(starts_with("Count")) 
   
-  scans.med <- select(scans.test, starts_with("Count"))
+  # Remove first two scans 
+  filtered.scans <- nthDelete(filtered.scans, 4, 1)
+  filtered.scans <- nthDelete(filtered.scans, 4, 2)
   
+  # Apply formula to scan data 
+  test2 <- scans.med
+  svp[,2:ncol(svp)]<-svp[,2:ncol(svp)]*((25.02*exp(-0.2382*svp$diameters))+(950.9*exp(-1.017*svp$diameters))+1)
+
+
+
   return(scan.graph.data)                                      
 }
+
 
 nmGraphData <- function(amplog.file) {
   nm.graph.data <- amplog.file %>% select(X1, X3) 
 }
+
+# Deletes every nth column in the given data frame.
+nthDelete <- function(data.frame, n, i) {
+  data.frame[,-(seq(i, to=ncol(data.frame), by=n))]
+}
+
